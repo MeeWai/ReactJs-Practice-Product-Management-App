7aa9a224a74e1fe64f7cd36647e1e477fae38e67
Mee Wai <nmw_ng@hotmail.com>
2021-09-06 18:00:24 +0800

    Initial commit

---
+++ src/ShowProducts/RetrieveProducts.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import ShowProduct from "./ShowProduct";
+import {useSnackbar} from "notistack";
+
+const RetrieveProducts = () =>{
+    const [products, getProducts] = useState("");
+    const {enqueueSnackbar} = useSnackbar();
+
+    const baseURL="http://localhost:8080/api/getAllProducts";
+
+    useEffect(()=>{
+        getAllProducts();
+    },[])
+
+    const getAllProducts = async () => {
+        try {
+           const response= await axios.get(baseURL);
+           const allProducts= response.data;
+           if(allProducts.length>0){
+               getProducts(allProducts);
+               enqueueSnackbar("Product load successfully.", {
+                   variant: 'success',
+               });
+           }else{
+               enqueueSnackbar("Product not found.", {
+                   variant: 'error',
+               });
+               console.log("Product not found.")
+           }
+        } catch (errorResponse) {
+            console.log(errorResponse);
+        }
+    }
+
+    return(
+        <ShowProduct productList={products}/>
+    )
+};
+
+export default RetrieveProducts;

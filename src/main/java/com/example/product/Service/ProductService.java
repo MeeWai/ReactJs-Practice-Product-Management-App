81eab92aacb5ada225211d352aaa2a518dd92c2f
Mee Wai <nmw_ng@hotmail.com>
2022-03-02 23:37:21 +0800

    Add Java

---
+++ src/main/java/com/example/product/Service/ProductService.java
@@ -0,0 +1,38 @@
+package com.example.product.Service;
+
+import com.example.product.Entity.Product;
+import com.example.product.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+//    public Iterable<Product> getAllProducts() {
+//        return productRepository.findAll();
+//    }
+
+    public Iterable<Product> getAllProducts(Pageable pageable) {
+
+        return productRepository.findAll(pageable);
+    }
+
+    public Product updateProduct(Product product) {
+        Product getProduct = productRepository.findById(product.getProductID()).orElse(null);
+
+        if (getProduct != null) {
+            getProduct.setProductName(product.getProductName());
+            getProduct.setProductPrice(product.getProductPrice());
+            getProduct.setProductQuantity(product.getProductQuantity());
+        }
+
+        return productRepository.save(product);
+    }
+}
